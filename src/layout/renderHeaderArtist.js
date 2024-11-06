296fb681fe7d6f59ff82bdad7591888d4c3fc62f
Marko <milevski.marko11@gmail.com>
2024-11-06 21:39:16 +0100

    Created artist home page

---
+++ src/layout/renderHeaderArtist.js
@@ -0,0 +1,43 @@
+export function renderHeaderArtistPage() {
+  let navBarClass;
+
+  switch (location.hash) {
+    case "#artistHomePage":
+      navBarClass = "artist-homepage--nav";
+      break;
+    case "#artistItemsPage":
+      navBarClass = "artist-items--nav";
+      break;
+    case "#addNewItemPage":
+      navBarClass = "add-new-item--nav";
+      break;
+    default:
+      navBarClass = "";
+  }
+
+  if (location.hash === "#artistHomePage") {
+    navBarClass = "artist-homepage--nav";
+  } else if (location.hash === "#artistItemsPage") {
+    navBarClass = "artist-items--nav";
+  } else if (location.hash === "#addNewItemPage") {
+    navBarClass = "add-new-item--nav";
+  }
+  return `
+    <nav class="nav-bar ${navBarClass}">
+        <a href="#landingPage">
+          <img src="./assets/logo.png" alt="logo" />
+        </a>
+        <h1 class="nav-bar--heading" id="artistName"></h1>
+        <div class="hamburger-icon" id="navToggle">
+          <i class="fas fa-bars"></i>
+        </div>
+      </nav>
+      <div class="nav-bar--dropdown" id="navDropdown">
+        <ul>
+          <li><a href="#artistHomePage">Home</a></li>
+          <li><a href="#artistItemsPage">Items</a></li>
+          <li><a href="#visitorHomePage">Auction</a></li>
+        </ul>
+      </div>
+    `;
+}
